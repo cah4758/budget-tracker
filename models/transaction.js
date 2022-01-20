bc744f5adc2e927ef849f25abf8e18b31c524331
Charles Hernandez <charlesh4758@gmail.com>
2022-01-20 14:40:24 -0600

    Model adjust for db matchup

--- models/transaction.js
+++ models/transaction.js
@@ -2,24 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  }
-);
+const transactionSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Transaction = mongoose.model("Transactions", transactionSchema);
 
 module.exports = Transaction;
